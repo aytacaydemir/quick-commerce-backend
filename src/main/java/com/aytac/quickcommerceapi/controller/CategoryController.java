4cef5eb5f044bde1eb30422a2f0722f9b01fa928
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-06 02:44:32 +0300

    code formatting and minor fixes

--- src/main/java/com/aytac/quickcommerceapi/controller/CategoryController.java
+++ src/main/java/com/aytac/quickcommerceapi/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.aytac.quickcommerceapi.controller;
 
 import com.aytac.quickcommerceapi.dto.PaginatedDataDto;
+import com.aytac.quickcommerceapi.dto.request.CategoryCreateRequest;
 import com.aytac.quickcommerceapi.dto.response.ApiResponse;
 import com.aytac.quickcommerceapi.dto.request.CategoryUpdateRequest;
 import com.aytac.quickcommerceapi.dto.response.CategoryResponse;
@@ -19,47 +20,58 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping(value = {"", "/"})
+    @GetMapping
     public ResponseEntity<ApiResponse<PaginatedDataDto<CategoryResponse>>> getAllCategories
             (@RequestParam(defaultValue = "0") int page,
              @RequestParam(defaultValue = "5") int size) {
         PaginatedDataDto<CategoryResponse> categories = categoryService.getAllCategories(page, size);
 
-        return new ResponseEntity<>(new ApiResponse<>(true, categories, null), HttpStatus.OK);
+        return new ResponseEntity<>(
+                new ApiResponse<>(true, categories, null), HttpStatus.OK);
+
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<CategoryResponse>> getCategoryById(@PathVariable(required = false) Long id) {
+    public ResponseEntity<ApiResponse<CategoryResponse>> getCategoryById(
+            @PathVariable(required = false) Long id) {
 
         CategoryResponse categoryResponseDto = categoryService.getCategoryById(id);
         if (categoryResponseDto != null) {
-            return new ResponseEntity<>(new ApiResponse<>(true, categoryResponseDto, null), HttpStatus.OK);
+            return new ResponseEntity<>(
+                    new ApiResponse<>(true, categoryResponseDto, null), HttpStatus.OK);
         }
         return new ResponseEntity<>(new ApiResponse<>(false, null, "404 - Category Not Found"), HttpStatus.NOT_FOUND);
     }
-
+*/
     @PostMapping
-    public void postCategory(@RequestParam String title, @RequestParam String image) {
-        categoryService.createCategory(title, image);
+    public ResponseEntity<ApiResponse<CategoryResponse>> postCategory(@RequestBody CategoryCreateRequest request) {
+
+        return new ResponseEntity<>(
+                new ApiResponse<>(true, categoryService.createCategory(request), null), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<?>> deleteCategoryById(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<CategoryResponse>> deleteCategoryById(@PathVariable Long id) {
         if (categoryService.deleteCategoryById(id)) {
-            return new ResponseEntity<>(new ApiResponse<>(true, null, null), HttpStatus.OK);
+            return new ResponseEntity<>(
+                    new ApiResponse<>(true, null, null), HttpStatus.OK);
         }
-        return new ResponseEntity<>(new ApiResponse<>(false, null, "404 - Category Not Found"), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(
+                new ApiResponse<>(false, null, "404 - Category Not Found"), HttpStatus.NOT_FOUND);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<CategoryResponse>> updateCategoryById(@RequestBody CategoryUpdateRequest request, @PathVariable Long id) {
+    public ResponseEntity<ApiResponse<CategoryResponse>> updateCategoryById(
+            @RequestBody CategoryUpdateRequest request, @PathVariable Long id) {
 
         CategoryResponse dto = categoryService.updateCategoryById(request, id);
 
         if (dto != null) {
-            return new ResponseEntity<>(new ApiResponse<>(true, dto, null), HttpStatus.CREATED);
+            return new ResponseEntity<>(
+                    new ApiResponse<>(true, dto, null), HttpStatus.CREATED);
         }
-        return new ResponseEntity<>(new ApiResponse<>(false, null, "404 - Category Not Found"), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(
+                new ApiResponse<>(false, null, "404 - Category Not Found"), HttpStatus.NOT_FOUND);
 
     }
 }
