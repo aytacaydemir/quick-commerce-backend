9e9d3d0b526a3c1b8e210e8133e7e160bd731a7b
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-08 18:30:43 +0300

    exception handler created

---
+++ src/main/java/com/aytac/quickcommerceapi/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.aytac.quickcommerceapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {CategoryNotFoundException.class, ProductNotFoundException.class})
+    public ResponseEntity<?> notFoundExceptionHandler(RuntimeException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
