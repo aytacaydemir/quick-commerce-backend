2d47205e35cea0111cce01bd5f4673399272dee4
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-29 02:49:52 +0300

    feat(productcontroller): Add createProduct method with Swagger summary and update API endpoint to /api/v1/products

--- src/main/java/com/aytac/quickcommerceapi/controller/ProductController.java
+++ src/main/java/com/aytac/quickcommerceapi/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.aytac.quickcommerceapi.controller;
 
 import com.aytac.quickcommerceapi.dto.ProductDto;
+import com.aytac.quickcommerceapi.dto.request.ProductCreateRequest;
 import com.aytac.quickcommerceapi.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -14,8 +17,16 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(summary = "Get Product Details by Product Id")
     @GetMapping("/{productId}")
-    public ProductDto getProductById(@PathVariable Long productId) {
-        return productService.getProductById(productId);
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long productId) {
+
+        return ResponseEntity.ok(productService.getProductById(productId));
+    }
+
+    @Operation(summary = "Create Product")
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductCreateRequest request) {
+        return ResponseEntity.ok(productService.createProduct(request));
     }
 }
