6767579ffc520fa32088f07e19edfb00ee161012
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-29 02:46:29 +0300

    feat(productservice): Implement createProduct method with injected SubcategoryService

--- src/main/java/com/aytac/quickcommerceapi/service/ProductService.java
+++ src/main/java/com/aytac/quickcommerceapi/service/ProductService.java
@@ -2,8 +2,10 @@ package com.aytac.quickcommerceapi.service;
 
 import com.aytac.quickcommerceapi.dto.ProductDto;
 import com.aytac.quickcommerceapi.dto.converter.ProductDtoConverter;
+import com.aytac.quickcommerceapi.dto.request.ProductCreateRequest;
 import com.aytac.quickcommerceapi.exception.ProductNotFoundException;
 import com.aytac.quickcommerceapi.model.Product;
+import com.aytac.quickcommerceapi.model.Subcategory;
 import com.aytac.quickcommerceapi.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +16,14 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductDtoConverter converter;
+    private final SubcategoryService subcategoryService;
 
-    public ProductService(ProductRepository productRepository, ProductDtoConverter converter) {
+    public ProductService(ProductRepository productRepository,
+                          ProductDtoConverter converter,
+                          SubcategoryService subcategoryService) {
         this.productRepository = productRepository;
         this.converter = converter;
+        this.subcategoryService = subcategoryService;
     }
 
     public ProductDto getProductById(Long productId) {
@@ -25,6 +31,23 @@ public class ProductService {
         Optional<Product> product = productRepository.findById(productId);
 
         return product.map(converter::convertToProductDto).orElseThrow(
-                () -> new ProductNotFoundException("Product not found by id= "+productId)); //product not found exception
+                () -> new ProductNotFoundException("Product not found by id= " + productId)); //product not found exception
+    }
+
+    public ProductDto createProduct(ProductCreateRequest request) {
+
+        Subcategory subcategory = subcategoryService.getSubcategoryById(request.subcategoryId());
+
+        Product toSave = new Product();
+        toSave.setTitle(request.title());
+        toSave.setShortTitle(request.shortTitle());
+        toSave.setSubcategory(subcategory);
+        toSave.setPrice(request.price());
+        toSave.setAltText(request.altText());
+        toSave.setImageData(request.imageData());
+
+        productRepository.save(toSave);
+
+        return converter.convertToProductDto(toSave);
     }
 }
