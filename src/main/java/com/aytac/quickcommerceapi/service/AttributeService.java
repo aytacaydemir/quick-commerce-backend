2279f10cf7279a8fabb522a468368bfd49ed82b7
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-30 16:00:57 +0300

    feat(attributeservice): Create AttributeService and getAttributeById method

---
+++ src/main/java/com/aytac/quickcommerceapi/service/AttributeService.java
@@ -0,0 +1,21 @@
+package com.aytac.quickcommerceapi.service;
+
+import com.aytac.quickcommerceapi.exception.AttributeNotFoundException;
+import com.aytac.quickcommerceapi.model.Attribute;
+import com.aytac.quickcommerceapi.repository.AttributeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AttributeService {
+
+    private final AttributeRepository attributeRepository;
+
+    public AttributeService(AttributeRepository attributeRepository) {
+        this.attributeRepository = attributeRepository;
+    }
+
+    public Attribute getAttributeById(Long id) {
+        return attributeRepository.findById(id)
+                .orElseThrow(() -> new AttributeNotFoundException("Attribute not found by id= " + id));
+    }
+}
