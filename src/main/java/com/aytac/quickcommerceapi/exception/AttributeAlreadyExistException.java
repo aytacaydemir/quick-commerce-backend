4c30dc2a7150131259b02387d2f32831ead8017c
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-30 15:50:26 +0300

    feat(exception): Create AttributeAlreadyExistException class

---
+++ src/main/java/com/aytac/quickcommerceapi/exception/AttributeAlreadyExistException.java
@@ -0,0 +1,8 @@
+package com.aytac.quickcommerceapi.exception;
+
+public class AttributeAlreadyExistException extends RuntimeException {
+
+    public AttributeAlreadyExistException(String message) {
+        super(message);
+    }
+}
