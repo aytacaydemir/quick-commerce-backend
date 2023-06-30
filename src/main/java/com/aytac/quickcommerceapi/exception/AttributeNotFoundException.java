0d56a204be6898dc3800473c9ad95a4269536179
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-30 15:50:41 +0300

    feat(exception): Create AttributeNotFoundException class

---
+++ src/main/java/com/aytac/quickcommerceapi/exception/AttributeNotFoundException.java
@@ -0,0 +1,10 @@
+package com.aytac.quickcommerceapi.exception;
+
+
+public class AttributeNotFoundException extends RuntimeException {
+
+    public AttributeNotFoundException(String message){
+        super(message);
+    }
+}
+
